f65a631965ad4f4f35d9a9fc11321065f6668140
lty <1392309506@qq.com>
2026-07-09 21:23:30 +0800

    chore: remove temp script

--- do_enc.py
+++
@@ -1,6 +0,0 @@
-import subprocess as s
-r = s.run(['git', 'add', '-A'], capture_output=True)
-r = s.run(['git', 'commit', '-m', 'fix: Windows GBK emoji auto-compat via src/__init__.py'], capture_output=True)
-print(r.stdout.decode()[:300])
-r = s.run(['git', 'push'], capture_output=True)
-print(r.stdout.decode()[:300])

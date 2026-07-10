6b317879c2ce4a8896f3d1374878ad1d7f162886
lty <1392309506@qq.com>
2026-07-10 20:18:48 +0800

    docs: merge 当前记录 进 审查报告，删除旧文件，更新引用

--- do_scripts.py
+++
@@ -1,4 +0,0 @@
-import subprocess as s
-s.run(['git','add','-A'], capture_output=True)
-s.run(['git','commit','-m','docs: scripts/README.md add run_simulation.py section'], capture_output=True)
-s.run(['git','push'])
